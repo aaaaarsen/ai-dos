3ed325fb8a05235370daae15ef788f46c4daa669
aaaaarsen <rayymbekarsen@gmail.com>
2026-06-17 01:56:15 +0500

    Initial go backend skeleton: gin + postgres connection + health endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"context"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbSslmode := os.Getenv("DB_SSLMODE")
+	serverPort := os.Getenv("SERVER_PORT")
+	
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", dbUser, dbPassword, dbHost, dbPort, dbName, dbSslmode) 
+
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		log.Fatalf("Connect failed: %v", err)
+	}
+	defer conn.Close(context.Background())
+	err = conn.Ping(context.Background())
+	
+	if err != nil {
+		log.Fatalf("Connect failed: %v", err)
+	}
+	log.Println("Database connected successfully")
+
+
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {c.JSON(200, gin.H{"status": "ok"})})
+	router.Run(":"+serverPort)
+}
