0a680e90a50db495054d140b6c57921f54c1239b
aaaaarsen <rayymbekarsen@gmail.com>
2026-07-13 17:30:44 +0500

    update: returining name in GetMe

--- internal/db/users.go
+++ internal/db/users.go
@@ -12,11 +12,11 @@ import (
 
 
 func CreateUser (pool *pgxpool.Pool, name *string, email string, passwordHash string) (*models.User, error) {
-	query := `INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id, created_at`
+	query := `INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id, name, created_at`
 
-	user := &models.User{Name: name ,Email: email, PasswordHash: passwordHash}
+	user := &models.User{}
 
-	err := pool.QueryRow(context.Background(), query, name, email, passwordHash).Scan(&user.ID, &user.CreatedAt)
+	err := pool.QueryRow(context.Background(), query, name, email, passwordHash).Scan(&user.ID, &user.Name, &user.CreatedAt)
 	if err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && pgErr.Code == "23505"{
@@ -25,15 +25,18 @@ func CreateUser (pool *pgxpool.Pool, name *string, email string, passwordHash st
 
 		return nil, err
 	}
+
+	user.Email = email
+	user.PasswordHash = passwordHash
 	return user, nil
 }
 
 func GetUserByEmail (pool *pgxpool.Pool, email string) (*models.User, error){
-	query := `SELECT id, email, password_hash, created_at FROM users WHERE email = $1`
+	query := `SELECT id, name, email, password_hash, created_at FROM users WHERE email = $1`
 
 	user := &models.User{}
 
-	err := pool.QueryRow(context.Background(), query, email).Scan(&user.ID, &user.Email, &user.PasswordHash, &user.CreatedAt)
+	err := pool.QueryRow(context.Background(), query, email).Scan(&user.ID,&user.Name, &user.Email, &user.PasswordHash, &user.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
@@ -41,11 +44,11 @@ func GetUserByEmail (pool *pgxpool.Pool, email string) (*models.User, error){
 }
 
 func GetUserByID (pool *pgxpool.Pool, id int64) (*models.User, error) {
-	query := `SELECT id, email, created_at FROM users where id = $1`
+	query := `SELECT id, name, email, created_at FROM users where id = $1`
 
 	user := &models.User{}
 
-	err := pool.QueryRow(context.Background(), query, id).Scan(&user.ID, &user.Email, &user.CreatedAt)
+	err := pool.QueryRow(context.Background(), query, id).Scan(&user.ID, &user.Name, &user.Email, &user.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
